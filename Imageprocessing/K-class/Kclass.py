c4106a48b399dcb5ae978612b494bccf0f8d76ba
zenghuichen <3045316072@qq.com>
2018-07-08 20:46:00 +0800

    add program for downLoad the images of ImageNet

--- Imageprocessing/K-class/Kclass.py
+++ Imageprocessing/K-class/Kclass.py
@@ -7,6 +7,7 @@ Created on Fri Jul  6 14:07:13 2018
 
 @author: 30453
 """
+#############################################模块调用区########################################################
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -18,7 +19,7 @@ import numpy as np
 #the module of purning model
 import csv
 from tensorflow.examples.tutorials.mnist import input_data
-os.environ['TF_CPP_LOG_LEVEL']='1'
+os.environ['TF_CPP_LOG_LEVEL']='1'  # 错误等级提示
 import sys
 from six.moves import urllib
 import tarfile
@@ -27,44 +28,38 @@ import cv2
 from PIL import Image as Image
 
 import scipy.signal as signal
-#程序主入口
+##############################################################################################################
 
-im=Image.open(r'./l8.jpg')
-#下面主要开始试验
-#拉普拉斯算子的结果
-laplacianKernel=np.array([[0,1,0],[1,-4,1],[0,1,0]],dtype=np.float)
-imarr=np.array(im,dtype=np.float)
-i_con=signal.convolve2d(imarr[:,:,0],laplacianKernel*10,mode='valid',boundary='fill',fillvalue=0)
-plt.imshow(i_con)
-plt.show()
-def maintest():
-    im=Image.open(r'./l8.jpg')
-    #下面主要开始试验
-    #拉普拉斯算子的结果
-    laplacianKernel=np.array([[0,1,0],[1,-4,1],[0,1,0]],dtype=np.float)
-    imarr=np.array(im,dtype=np.float)
-    i_con=signal.convolve2d(imarr[:,:,0],laplacianKernel*10,mode='valid',boundary='fill',fillvalue=0)
-    plt.imshow(i_con)
-# 在原有图片矩阵的基础上完成了相关运算的加载，注意，这里使用tensorflow的方案
-def createLOGKernel(sigma,size):
-    H,W=size
-    r,c=np.mgrid[0:H:1,0:W:1]
-    r=r-(H-1)/2
-    c=c-(W-1)/2
-    #方差
-    sigma2=pow(sigma,2.0)
-    norm2=np.power(r,2.0)+np.power(c,2.0)
-    LOGKernel=(norm2/sigma2-2)*np.exp(-norm2/(2*sigma2))
-    return LOGKernel
 
+############################进行滤波方法区，主要是算法前预处理####################################################
+
+
+
+#高斯拉普拉斯
 def LoG(image,sigma,size,_boundary='symm',_mode='valid'):
+    def createLOGKernel(sigma,size):
+        H,W=size
+        r,c=np.mgrid[0:H:1,0:W:1]
+        r=r-(H-1)/2
+        c=c-(W-1)/2
+        #方差
+        sigma2=pow(sigma,2.0)
+        norm2=np.power(r,2.0)+np.power(c,2.0)
+        LOGKernel=(norm2/sigma2-2)*np.exp(-norm2/(2*sigma2))
+        return LOGKernel
     loGKernl=createLOGKernel(sigma,size)
     im_log=signal.convolve2d(image,loGKernl,mode=_mode,boundary=_boundary)
     return im_log
-def ImgProProcessing(path):
-    im=Image.open(path)
-    #先进行边缘滤波
-    
+#拉普拉斯算子
+def lapalcian(image):
+    laplacianKernel=np.array([[0,1,0],[1,-4,1],[0,1,0]],dtype=np.float)
+    i_con=signal.convolve2d(image,laplacianKernel*10,mode='valid',boundary='fill',fillvalue=0)
+    return  i_con
+
+def sobel_v(image):
+    laplacianKernel=np.array([[0,1,0],[1,-4,1],[0,1,0]],dtype=np.float)
+    i_con=signal.convolve2d(image,laplacianKernel*10,mode='valid',boundary='fill',fillvalue=0)
+    return  i_con
 
 
 #将输入的图片解析成相关的向量集 
@@ -106,5 +101,18 @@ def getVectList(path):
             j=j+1
         i=i+1
     return result
+##############################################################################################################
 
+#########################################程序主要代码测试区域###################################################
+
+im=Image.open(r'./l8.jpg')# 读取图片
+imarr=np.array(im,dtype=np.float)
+i_con=lapalcian(imarr[:,:,0])
+plt.imshow(i_con)
+plt.show()
+i_log=LoG(imarr[:,:,0],6,(3,3))
+plt.imshow(i_log)
+plt.show()
+
+##############################################################################################################
     
---
+++ Imageprocessing/getImages/getImage/getImage/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getImage
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+         
+            FileStream fs = new FileStream(urllist.boxsyepath, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string r1 = "";
+            while (r1!=null)
+            {
+                r1 = sr.ReadLine();
+                Console.Write(r1);
+            }
+            Console.WriteLine(r1);
+
+            sr.Close();
+            fs.Close();
+            Console.ReadLine();
+        }
+
+
+    }
+
+    public class ImageUpDown
+    {
+        public string getOne(FileStream fs)
+        {
+            string result = "";
+
+            return result;
+        }
+        public  string[] CreateNlist()
+        {
+            string[] list =new string[4000];
+            //开始解析相关的程序代码
+            FileStream fs = new FileStream(urllist.boxsyepath, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string r1 = "";
+            while (r1 != null)
+            {
+                r1 = sr.ReadLine();
+                Console.Write(r1);
+            }
+            sr.Close();
+            fs.Close();
+            return list;
+        }
+        public int getxxxx(string xxxx)
+        {
+            int i = 0;
+            string num = xxxx.Trim().Substring(1);
+            return i;
+        }
+       
+    }
+    public class meta
+    {
+        public string Synerl;
+        public string id;
+        public string url;
+    }
+    public class urllist
+    {
+        public static string boxurl = @"http://www.image-net.org/api/download/imagenet.bbox.synset?wnid=";
+        public static string fall11path = @"E:\神经网络\imagenet_fall11_urls\fall11_urls.txt";
+        public static string boxsyepath = @"E:\神经网络\imagenet_fall11_urls\boxlist.txt";
+    }
+
+}
+
+
+
+
