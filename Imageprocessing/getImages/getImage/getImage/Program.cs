cc3f5d5c5b089b3234ed43f6fc5dec885d53cc74
zenghuichen <3045316072@qq.com>
2018-07-08 22:33:30 +0800

    the find work is over

--- Imageprocessing/getImages/getImage/getImage/Program.cs
+++ Imageprocessing/getImages/getImage/getImage/Program.cs
@@ -13,20 +13,14 @@ namespace getImage
        
         static void Main(string[] args)
         {
-         
-            FileStream fs = new FileStream(urllist.boxsyepath, FileMode.Open, FileAccess.Read);
-            StreamReader sr = new StreamReader(fs);
-            string r1 = "";
-            while (true)
-            {
-                r1 = sr.ReadLine();
-                if (r1 == null) break;
-                int i = ImageUpDown.getxxxx(r1);
-                Console.Write(i.ToString());
-            }
-            Console.WriteLine(r1);
-            sr.Close();
-            fs.Close();
+            ImageUpDown down = new ImageUpDown();
+            string[] list = down.CreateNlist();
+            //测试寻址性能
+            string test = "n13044778";
+            Console.WriteLine(test);
+            int t = down.Find(test);
+            Console.WriteLine(down.list[t]);
+            Console.WriteLine(t.ToString());
             Console.ReadLine();
         }
 
@@ -35,7 +29,8 @@ namespace getImage
 
     public class ImageUpDown
     {
-        public string getOne(FileStream fs)
+        public string[] list=new string[4000];
+        public string GetOne(FileStream fs)
         {
             string result = "";
 
@@ -50,22 +45,44 @@ namespace getImage
             string r1 = "";
             while (true)
             {
+
                 r1 = sr.ReadLine();
                 if (r1 == null) break;
-                int i = getxxxx(r1);
+                int i = Getxxxx(r1);//
+                int index = i % 4000;
+                while (list[index]!=null)
+                {
+                    i = i + 1;
+                    index = i % 4000;
+                }
+                list[index] = r1;
             }
             sr.Close();
             fs.Close();
+            this.list = list;
             return list;
         }
-        public static int getxxxx(string xxxx)
+        public static int Getxxxx(string xxxx)
         {
             int i = 0;
             string num = xxxx.Trim().Substring(1);
             i = int.Parse(num);
             return i;
         }
-       
+        public int Find(string n)
+        {
+            int t = Getxxxx(n);
+            int index = t%4000;
+            while (n != this.list[index]&&this.list[index]!=null)
+            {
+                t = t + 1;
+                index = t % 4000;
+                i = i + 1;
+            }
+            if (this.list[index] == null) return -1;
+            if (this.list[index] == n) return index;
+            return -1;
+        }
     }
     public class meta
     {
